7a842775618e2f20e0e8e4722bba8fefd38f022e
Patrick Hulin <patrick.hulin@gmail.com>
2021-01-02 17:35:22 -0500

    Fix decorator stuff for libram import.

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,15 @@ module.exports = function (api) {
         },
       ],
     ],
-    plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-object-rest-spread'],
+    plugins: [
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          decoratorsBeforeExport: true,
+        },
+      ],
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+    ],
   };
 };
